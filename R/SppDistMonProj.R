b8d769a5faf31fce2a0f2bdf0a7d4571abb807ac
McCrea Cobb <30300184+mccrea-cobb@users.noreply.github.com>
2020-03-04 08:57:37 -0900

    Added additional documentation (R/SppDistMonProj.R) (#1)
    * Adds SppDistMonProj.R package documentation
    
    * Adds vignette template. Updates gitignore.
    
    * updates vignette and documentation

---
+++ R/SppDistMonProj.R
@@ -0,0 +1,25 @@
+#' SppDistMonProj: a package for learning data workflow concepts with a Species Distribution Monitoring Project
+#'
+#' The \code{SppDistMonProj} package contains a suite of functions for suite of R
+#' functions used for simulating, processing, and analyzing data from an
+#' example survey project.
+#'
+#' @section Simulation functions:
+#' The simulation functions include \code{create.raster}, \code{create.site.data},
+#' \code{sim.occu.data}, \code{df2s123df}, and \code{one.step}.
+#'
+#' @section Quality control (QC) functions:
+#' The QC functions ...
+#'
+#' @section Analysis functions:
+#' The analysis functions ...
+#'
+#' @section Plot functions:
+#' The plot functions ...
+#'
+#' @section Misc functions:
+#' Other functions ...
+#'
+#' @docType package
+#' @name SppDistMonProj
+NULL
